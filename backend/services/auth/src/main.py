ee1707a016d3c0004b16952d77b23d6e79146321
Кирилл Курец <reiko@MacBook-Pro--Kiryl.local>
2025-08-06 09:18:56 +0200

    updated .gitignore

---
+++ backend/services/auth/src/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Auth",
+    version="0.0.1",
+)
---
+++ backend/services/user/src/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="User",
+    version="0.0.1",
+)
